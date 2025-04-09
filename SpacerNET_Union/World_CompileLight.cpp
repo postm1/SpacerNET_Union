c183e438b6d293cfd6871cf32cde2a05783d87e9
proxin <postm1@yandex.ru>
2025-04-09 11:55:26 +0300

    Improve zMSH_VERTNORMAL_SMOOTH

--- SpacerNET_Union/World_CompileLight.cpp
+++ SpacerNET_Union/World_CompileLight.cpp
@@ -9,18 +9,33 @@ namespace GOTHIC_ENGINE {
 	HOOK Ivk_zCMesh_CalcVertexNormals AS(&zCMesh::CalcVertexNormals, &zCMesh::CalcVertexNormals_Union);
 	void zCMesh::CalcVertexNormals_Union(zTCalcVertNormalMode mode, zCBspTree* bspTree)
 	{
+		if (!bspTree)
+		{
+			cmd << "CalcVertexNormals: original" << endl;
+
+			RX_Begin(10);
+			THISCALL(Ivk_zCMesh_CalcVertexNormals)(mode, bspTree);
+
+			RX_End(10);
+
+
+			cmd << "CalcVertexNormals: " << (int)mode << " " << RX_PerfString(10) << endl;
+
+			return;
+		}
+
 		zTBBox3D	polyBBox;
 		zCPolygon** foundPolyList;
 		int			foundPolyNum;
 
 
-		cmd << "CalcVertexNormals: " << (int)mode << " started..." << endl;
+		cmd << "CalcVertexNormalsNew: Mode: " << (int)mode << " BSP: " << (int)bspTree << " started..." << endl;
 
 		ArraysToLists();
 		UnshareFeatures();
 
 		RX_Begin(10);
-		//THISCALL(Ivk_zCMesh_CalcVertexNormals)(mode, bspTree);
+		//
 
 		if (mode == zMSH_VERTNORMAL_MAT) //LightWorldStaticCompiled
 		{
@@ -67,28 +82,6 @@ namespace GOTHIC_ENGINE {
 									};
 								};
 							}
-							else
-							{
-								// welches Poly teilt noch dieses aktuelle Vert ?
-								for (int k = 0; k < numPoly; k++)
-								{
-									zCPolygon* poly2 = polyList[k];
-
-									if (poly2->material == poly->material)
-									{
-										if (poly2->VertPartOfPoly(vert))
-										{
-											if (i == k) continue;
-											// 2. Poly gefunden: ist dieses
-											// a) auch ein Smooth-Material b) ist der Winkel < alpha° ?
-											zREAL smoothRel = zMax(poly->GetMaterial()->smoothAngle, poly2->GetMaterial()->smoothAngle);
-											zREAL polyRel = Alg_Rad2Deg(Alg_AngleUnitRad(poly->GetNormal(), poly2->GetNormal()));
-											if (smoothRel > polyRel)
-												feat->vertNormal += poly2->GetNormal();
-										};
-									};
-								};
-							};
 							// hier kann theoret. eine Normale aus obiger Berechnung mit Laenge 0 ankommen
 							// sollte man das anders behandeln .. ?
 							zREAL normalLen = feat->vertNormal.Length();
@@ -100,9 +93,25 @@ namespace GOTHIC_ENGINE {
 		}
 		else if (mode == zMSH_VERTNORMAL_SMOOTH) // light
 		{
+			// Предварительно строим карту вершин
+			std::unordered_map<zCVertex*, std::vector<zCPolygon*>> vertexToPolygonsMap;
 
-			for (int i = 0; i < numPoly; i++) {
+			vertexToPolygonsMap.reserve(numPoly);
+
+			for (int i = 0; i < numPoly; i++) 
+			{
 				zCPolygon* poly = polyList[i];
+
+				for (int j = 0; j < poly->polyNumVert; j++) 
+				{
+					vertexToPolygonsMap[poly->vertex[j]].push_back(poly);
+				}
+			}
+
+			for (int i = 0; i < numPoly; i++) 
+			{
+				zCPolygon* poly = polyList[i];
+
 
 				if (bspTree)
 				{
@@ -113,6 +122,12 @@ namespace GOTHIC_ENGINE {
 					if (polyBBox.mins[VY] == polyBBox.maxs[VY]) { polyBBox.mins[VY] -= INC; polyBBox.maxs[VY] += INC; };
 					if (polyBBox.mins[VZ] == polyBBox.maxs[VZ]) { polyBBox.mins[VZ] -= INC; polyBBox.maxs[VZ] += INC; };
 				};
+
+				bspTree->bspRoot->CollectPolysInBBox3D(polyBBox, foundPolyList, foundPolyNum);
+
+				int saveNum = foundPolyNum;
+				//cmd << "CommonResult: " << polyBBox.mins.ToString() + "/" + polyBBox.maxs.ToString() << " foundPolyNum: " << foundPolyNum << endl;
+
 				for (int j = 0; j < poly->polyNumVert; j++)
 				{
 					zCVertex* vert = poly->vertex[j];
@@ -121,29 +136,51 @@ namespace GOTHIC_ENGINE {
 					feat->vertNormal = poly->GetNormal();
 					if (bspTree)
 					{
-						bspTree->bspRoot->CollectPolysInBBox3D(polyBBox, foundPolyList, foundPolyNum);
+						
+						//bspTree->bspRoot->CollectPolysInBBox3D(polyBBox, foundPolyList, foundPolyNum);
+
+						//cmd << "\tPartResult: [" << j << "] " << " foundPolyNum: " << foundPolyNum << endl;
+
+						/*if (foundPolyNum != saveNum)
+						{
+							cmd << "!!!!!!!!!!!!! " << foundPolyNum << " / " << saveNum << endl;
+						}*/
+						
+
 						// welches Poly teilt noch dieses aktuelle Vert ?
 						for (int k = 0; k < foundPolyNum; k++)
 						{
 							zCPolygon* poly2 = foundPolyList[k];
+
+							//some debug test code
+							//bool flagFound = false;
+							//bool flagFound2 = false;
+
+							/*
 							if (poly2->VertPartOfPoly(vert))
 							{
 								if (poly2 != poly)
-									feat->vertNormal += poly2->GetNormal();
+								{
+									//feat->vertNormal += poly2->GetNormal();
+									flagFound = true;
+								}
 							};
+							*/
+							
+
+							if (poly2 != poly && std::find(poly2->vertex, poly2->vertex + poly2->polyNumVert, vert) != poly2->vertex + poly2->polyNumVert)
+							{
+								feat->vertNormal += poly2->GetNormal();
+								//flagFound2 = true;
+							}
+
+							/*if (flagFound != flagFound2)
+							{
+								cmd << "flagFound fail! " << flagFound << "/" << flagFound2 << endl;
+							}*/
 						};
 					}
-					else
-					{
-						// welches Poly teilt noch dieses aktuelle Vert ?
-						for (int k = 0; k < numPoly; k++)
-						{
-							zCPolygon* poly2 = polyList[k];
-							if ((poly2->VertPartOfPoly(vert) && (i != k))) {
-								feat->vertNormal += poly2->GetNormal();
-							};
-						};
-					};
+
 					feat->vertNormal.NormalizeSafe();
 				};
 			};
@@ -158,7 +195,7 @@ namespace GOTHIC_ENGINE {
 					poly->feature[j]->vertNormal = poly->GetNormal();
 			};
 			
-		}; /* END FACET */
+		};
 		
 		
 		RX_End(10);
