21006adfe02e63d019babad23ac895cfefa00bf6
proxin <postm1@yandex.ru>
2025-04-14 15:27:37 +0300

    Output fix

--- SpacerNET_Union/VobManipulator.cpp
+++ SpacerNET_Union/VobManipulator.cpp
@@ -1351,11 +1351,11 @@ namespace GOTHIC_ENGINE {
 
 			if (value)
 			{
-				print.PrintGreen(GetLang("CHANGE_VOB_COMPLEX_MOVE") + " " + GetLang("COMMON_TURN_ON"));
+				print.PrintGreen(GetLang("CHANGE_VOB_COMPLEX_MOVE") + ": " + GetLang("COMMON_TURN_ON"));
 			}
 			else
 			{
-				print.PrintRed(GetLang("CHANGE_VOB_COMPLEX_MOVE") + " " + GetLang("COMMON_TURN_OFF"));
+				print.PrintRed(GetLang("CHANGE_VOB_COMPLEX_MOVE") + ": " + GetLang("COMMON_TURN_OFF"));
 			}
 
 			theApp.options.SetIntVal("checkBoxBlockComplexVob", value);
