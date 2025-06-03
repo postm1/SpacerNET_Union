c8d780a50ad80c4061bb2a5b1a08539ac9006321
proxin <postm1@yandex.ru>
2025-06-03 13:47:21 +0300

    Test code for search wind vobs...

--- SpacerNET_Union/TestLoop.cpp
+++ SpacerNET_Union/TestLoop.cpp
@@ -32,9 +32,140 @@ namespace GOTHIC_ENGINE {
 		cmd << "AFTER: " << trafo.ToString().c_str() << endl;
 	}
 
+
+	std::unordered_map<std::string, float> mapValues;
+
+	void ReadFileData()
+	{
+		cmd << "Reading from file ./system/WindObjects.txt" << endl;
+
+		std::ifstream inputFile("./system/WindObjects.txt");
+
+		if (!inputFile.is_open())
+		{
+			cmd << "Error: no file found in system folder" << endl;
+			return;
+		}
+
+
+		std::string line;
+		while (std::getline(inputFile, line)) {
+
+			if (line.empty() || (line.substr(0, 2) == "//")) {
+				continue;
+			}
+
+
+			size_t delimiterPos = line.find('|');
+			if (delimiterPos == std::string::npos) {
+				continue;
+			}
+
+			std::string name = line.substr(0, delimiterPos);
+			std::string valueStr = line.substr(delimiterPos + 1);
+
+			
+			if (mapValues.find(name) != mapValues.end())
+			{
+				cmd << "BAD VALUE DUBLI: " << name.c_str() << endl;
+
+			}
+			else
+			{
+				float value = std::stof(valueStr);
+				mapValues[name] = value;
+			}
+		}
+
+		inputFile.close();
+
+
+		//cmd << "Reading OK: " << mapValues.size() << endl;
+
+		/*
+		for (const auto& pair : mapValues)
+		{
+			cmd << pair.first.c_str() << " | " << pair.second << endl;
+		}
+		*/
+	}
+
 	// test loop, some test code always here...
 	void SpacerApp::TestLoop()
 	{
+
+		if (zinput->KeyPressed(KEY_F1))
+		{
+			zinput->ClearKeyBuffer();
+
+			mapValues.clear();
+			debug.CleanLines();
+			ReadFileData();
+			print.PrintGreen("OOOK");
+
+			zCArray<zCVob*> pList;
+
+			ogame->GetWorld()->SearchVobListByBaseClass(zCVob::classDef, pList, 0);
+
+			int countRemoved = 0;
+
+			for (int i = 0; i < pList.GetNumInList(); i++)
+			{
+				zCVob* pVob = pList.GetSafe(i);
+
+				if (pVob) {
+
+					if (pVob->CastTo<zCTriggerBase>() || pVob->CastTo<zCZone>())
+					{
+						continue;
+					}
+
+					if (pVob->GetVisual() && pVob->showVisual && !pVob->HasChildren())
+					{
+						if (pVob->GetVisual()->GetVisualName().Contains(".3DS"))
+						{
+							if (mapValues.find(pVob->GetVisual()->GetVisualName().ToChar()) != mapValues.end())
+							{
+								RemoveVob(pVob);
+								countRemoved++;
+							}
+						}
+					}
+				}
+			}
+			
+			cmd << "Removed: " << countRemoved << endl;
+
+			std::unordered_set<std::string> foundVobs;
+
+			int count = 0;
+
+			for (int i = 0; i < pList.GetNumInList(); i++) {
+
+				zCVob* pVob = pList.GetSafe(i);
+
+				if (pVob) {
+
+					if (pVob->GetVisual() && pVob->showVisual)
+					{
+						if (pVob->GetVisual()->GetVisualName().Contains(".3DS"))
+						{
+							if (mapValues.find(pVob->GetVisual()->GetVisualName().ToChar()) == mapValues.end() && foundVobs.find(pVob->GetVisual()->GetVisualName().ToChar()) == foundVobs.end())
+							{
+								foundVobs.insert(pVob->GetVisual()->GetVisualName().ToChar());
+
+								auto pos = pVob->GetPositionWorld();
+
+								debug.AddLine(pos, pos + zVEC3(0, 5000, 0), GFX_RED, 1000e3);
+								
+							}
+						}
+					}
+
+				}
+			}
+		}
+
 		/*if (auto pVob = GetSelectedVob())
 		{
 			if (zinput->KeyPressed(KEY_F1))
